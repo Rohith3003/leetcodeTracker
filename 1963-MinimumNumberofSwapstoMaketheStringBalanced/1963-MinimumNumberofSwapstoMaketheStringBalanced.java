739e8ba59a2af3075837391fadca4d397c852a0a
Rohith3003 <100356094+Rohith3003@users.noreply.github.com>
2025-02-22 23:45:40 +0530

    Create file

---
+++ 1963-MinimumNumberofSwapstoMaketheStringBalanced/1963-MinimumNumberofSwapstoMaketheStringBalanced.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minSwaps(String s) {
+        int openCount=0;
+        int closedCount=0;
+        int swaps=0;
+
+        for(char c: s.toCharArray()) {
+            if(c=='[') {
+                openCount++;
+            } else if(c==']') {
+                closedCount++;
+            }
+
+            if(closedCount>openCount) {
+                swaps++;
+                openCount++;
+                closedCount--;
+            }
+        }
+
+        return swaps;
+    }
+}
