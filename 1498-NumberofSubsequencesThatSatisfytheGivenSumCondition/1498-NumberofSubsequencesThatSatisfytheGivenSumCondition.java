cdc7057292f0974b84017c78287f99f52664726e
Rohith3003 <100356094+Rohith3003@users.noreply.github.com>
2025-03-03 23:44:26 +0530

    Create file

---
+++ 1498-NumberofSubsequencesThatSatisfytheGivenSumCondition/1498-NumberofSubsequencesThatSatisfytheGivenSumCondition.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        int start=0, end=n-1;
+        int count=0;
+
+        int mod = 1_000_000_007;
+
+        int[] power = new int[n];
+        power[0] = 1;
+
+        for(int i=1;i<n;i++) {
+            power[i] =  (power[i-1]*2)%mod;
+        }
+
+        while(start<=end) {
+            if(nums[start]+nums[end]<=target) {
+                count = (count+power[end-start])%mod;
+                start++;
+            } else {
+                end--;
+            }
+        }
+
+        return count%mod;
+    }
+}
