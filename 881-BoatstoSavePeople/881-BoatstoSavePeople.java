21aa1457cbff3286a08f0b714baeeab2dfc1f29f
Rohith3003 <100356094+Rohith3003@users.noreply.github.com>
2025-03-05 22:22:40 +0530

    Create file

---
+++ 881-BoatstoSavePeople/881-BoatstoSavePeople.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        
+        Arrays.sort(people);
+        int n = people.length-1;
+        int start=0, end=n;
+        int reqBoats=0;
+
+        while(start<=end) {
+            if(people[start]+people[end]>limit) {
+                end--;
+            } else {
+                start++;
+                end--;
+            }
+            reqBoats++;
+        }
+
+        return reqBoats;
+    }
+}
