688905a062a1a727ff1578094ebf4fe790934bf5
Rohith3003 <100356094+Rohith3003@users.noreply.github.com>
2025-03-10 22:41:29 +0530

    File updated at 3/10/2025, 10:41:29 PM

--- 1578-MinimumTimetoMakeRopeColorful/1578-MinimumTimetoMakeRopeColorful.java
+++ 1578-MinimumTimetoMakeRopeColorful/1578-MinimumTimetoMakeRopeColorful.java
@@ -1,12 +1,13 @@
-// Last updated: 3/10/2025, 10:39:58 PM
+// Last updated: 3/10/2025, 10:41:29 PM
 class Solution {
     public int minCost(String colors, int[] neededTime) {
         int totalTime = 0;
         int sum = neededTime[0];
         int maxCost = neededTime[0];
+        char[] colorsArr = colors.toCharArray();
 
         for (int i = 1; i < colors.length(); i++) {
-            if (colors.charAt(i) == colors.charAt(i - 1)) { 
+            if (colorsArr[i] == colorsArr[i-1]) { 
                 sum += neededTime[i];
                 maxCost = Math.max(maxCost, neededTime[i]);
             } else {
