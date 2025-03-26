99b6eae2d98781bec0bb6318cc1b40777529288c
Rohith3003 <100356094+Rohith3003@users.noreply.github.com>
2025-03-26 09:02:33 +0530

    Create file

---
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.java
@@ -0,0 +1,30 @@
+// Last updated: 3/26/2025, 9:02:33 AM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+
+        while (p1 != p2) {
+            // Move to next node or switch to the other list
+            p1 = (p1 == null) ? headB : p1.next;
+            p2 = (p2 == null) ? headA : p2.next;
+        }
+
+        return p1;
+    }
+}
